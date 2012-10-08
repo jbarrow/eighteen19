c5c94214868e446e6d0a56678c7e8c2e1ef6154e
Joseph Barrow <joseph.d.barrow@gmail.com>
2012-10-08 03:00:00 -0400

    Finished Authentication

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ var express = require('express')
 var database = "1819";
 var collections = ["users", "keys"];
 var db = require("mongojs").connect(database, collections);
+var users = db.collection('users');
+var keys = db.collection('keys');
 
 /**
  * Password Hashing Data
@@ -25,20 +27,18 @@ var db = require("mongojs").connect(database, collections);
 var strLen = 128;
 var iterations = 12000;
 
-function hash(pwd, salt, fn) {
-	if (arguments.length == 3) {
-		crypto.pbkdf2(pwd, sald, iterations, len, fn);
-	} else {
-		fn = salt;
-		crypto.randomBytes(len, function(err, salt) {
-			if (err) return fn(err);
-			salt = salt.toString('base64');
-			crypto.pbkdf2(pwd, salt, iterations, len, function(err, hash) {
-				if (err) return fn(err);
-				fn(null, salt, hash);
-			});
-		});
-	}
+function generateSalt(callback) {
+	crypto.randomBytes(64, function(err, buf) {
+		if (err) throw err;
+		callback(buf.toString('hex'));
+	});
+}
+
+function hash(pwd, salt, callback) {
+	crypto.pbkdf2(pwd, salt, iterations, strLen, function(err, password) {
+		if (err) throw err;
+		callback(password);
+	});
 }
 
 /**
@@ -78,30 +78,32 @@ app.configure('development', function(){
 });
 
 function authenticate(username, pass, captcha, os, browser, fn) {
-	console.log(username);
-	console.log(pass);
-	console.log(os);
-	console.log(browser);
-	var user = db.users.findOne({ 'username' : username }, function(err, user) {
-		console.log(err);
-		console.log(user);
+	var secure = db.keys.findOne({ 'key' : pass }, function(err, key) {
+		// Email!
+		generateSalt(function(hash) {
+			db.keys.update({ 'key' : key.key }, { $set : { 'hash' : hash, 'forward' : true }});
+			return;
+		});
 	});
-	if(!user) return fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
 
-	if(user.os != '' && user.browser != '') {
-		hash(pass, user.salt, function(err, hash) {
-			if(err) return fn(err);
-			if(hash == user.hash) return fn(null, user);
-			fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
-		})
-	} else {
-		hash(pass, user.salt, function(err, hash) {
-			if(err) return fn(err);
-			if(hash == user.hash && os = user.os && browser = user.browser) return fn(null, user);
-			fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
-		})
-	}
+	var user = db.users.findOne({ 'username' : username, 'captcha' : captcha }, function(err, user) {
+		if(!user) return fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
 
+		if(!user.os || !user.browser) {
+			hash(pass, user.salt, function(hash) {
+				if(hash == user.hash) { 
+					db.users.update({ 'username' : user.username, 'hash' : user.hash }, { $set : { 'browser' : browser, 'os' : os } });
+					return fn(null, user);
+				}
+				return fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
+			});
+		} else {
+			hash(pass, user.salt, function(hash) {
+				if(hash == user.hash && os == user.os && browser == user.browser) return fn(null, user);
+				return fn(new Error('Looks like something went wrong.  Different computer?  Bad password?'));
+			});
+		}
+	});
 }
 
 function restrict(req, res, next) {
@@ -114,11 +116,11 @@ function restrict(req, res, next) {
 }
 
 app.get('/', function(req, res) {
-	res.render('index', { username: '', captcha: '' });
+	res.render('index', { username: '', captcha: '', message: '' });
 });
 
 app.get('/login', function(req, res) {
-	res.render('index', { username: '', captcha: '' });
+	res.render('index', { username: '', captcha: '', message: '' });
 });
 
 app.get('/logout', function(req, res) {
@@ -127,23 +129,31 @@ app.get('/logout', function(req, res) {
 	});
 });
 
+app.get('/request', function(req, res) {
+
+});
+
+app.post('/request', function(req, res) {
+
+});
+
 app.post('/login', function(req, res) {
 	var ua = uaParser.parse(req.headers['user-agent']);
 	authenticate(req.body.username, req.body.password, req.body.captcha, ua.os, ua.family, function(err, user) {
 		if(user) {
 			req.session.regenerate(function(){
 				req.session.user = user;
-				req.session.success = 'Congratulations!';
 				res.redirect('files');
 			});
 		} else {
-			req.session.error = 'Something went wrong.';
-			res.render('index', { username: req.body['username'], captcha: req.body['captcha'] });
+			res.render('index', { username: req.body['username'], captcha: req.body['captcha'], message: err });
 		}
 	});	
 });
 
-app.get('/files', function(req, res) {});
+app.get('/files', function(req, res) {
+	res.render('index', { username: '', captcha: 'It Worked!', message: 'Congratulations, motherfucker!' });
+});
 
 app.get('/authorize/:hash', function(req, res) {});
 
